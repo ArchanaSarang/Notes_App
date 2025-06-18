a160b3dd6f8af399840eee0acee2f0b9a8f97e17
ArchanaSarang <archana.sarang@gmail.com>
2025-06-18 20:37:45 +0530

    Set links by using Navlink to Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
-const Navbar = () =>{
-    return(
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return (
         <>
-        <h1>Navbar</h1>
+            {/* <h1>Navbar</h1> */}
+            <div className="d-flex justify-content-center align-items-center gap-3 my-3">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/notes">Notes</NavLink>
+            </div>
+
+
         </>
     );
 }
