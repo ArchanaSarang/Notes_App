55fc6dec24b4c9df496e7812d9ffd9c5075a8d68
ArchanaSarang <archana.sarang@gmail.com>
2025-06-19 11:38:02 +0530

    addjust layout of home page, logic of button lables (create/update)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 const Home = () => {
-    return(
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState('');
+    const [searchParams, setsearchParams] = useSearchParams();
+    const noteId = searchParams.get('noteId');
+    return (
         <>
-        <h1>Home</h1>
+
+            <div className="d-flex flex-column justify-content-center">
+                <div className="d-flex justify-content-center">
+                    <input className="from-control w-50 my-3 border border-2 border-primary rounded-3 p-2"
+                        type="text"
+                        placeholder="Enter title here"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <button className="btn btn-primary mx-2 my-3 rounded-3 p-2">
+                        {
+                            noteId ? 'Update My Note ' : 'Create My Note'
+                        }
+                    </button>
+
+                </div>
+                <div className="d-flex justify-content-center">
+                    <textarea
+                        className="form-control w-75 my-3 border border-2 border-primary rounded-3 p-2"
+                        value={value}
+                        placeholder="Enter your note here"
+                        onChange={(e) => setValue(e.target.value)}
+
+                    />
+                </div>
+            </div>
+
+            {/* <h1>Home</h1> */}
+
+
         </>
     );
 }
